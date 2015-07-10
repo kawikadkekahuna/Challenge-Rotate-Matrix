6d919c24dbb3de2519ee58d15b0f62794c9ff76e
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-07-10 09:32:22 -1000

    added second layer to move

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -21,24 +21,23 @@ function MatrixRotator(matrix) {
 MatrixRotator.prototype.rotate = function(direction) {
 
 
-  var matrixDimensions = this.matrix.length;
   var SCHEMA;
 
   function loop(matrix) {
-    var testMatrix = assignValues(matrix);
+    var testMatrix = assignValues(matrix, 1, matrix.length);
     console.log('testMatrix', testMatrix);
-    testMatrix = turnCW(testMatrix)
-    console.log('testMatrix', testMatrix);
+    testMatrix = turnCW(testMatrix, 1, testMatrix.length);
+    console.log('turnCW', testMatrix);
     testMatrix = matchMatrixToSchema(testMatrix);
-    console.log('testMatrix', testMatrix);
-    // var d = assignValues(matrix, 1);
-    // var step = turnCW(d, 1);
+    console.log('matchMatrix', testMatrix);
+
   }
 
   loop(this.matrix);
 
-  function assignValues(matrix, layer) {
+  function assignValues(matrix, layer, dimensions) {
     var schema = {};
+    var matrixDimensions = dimensions;
     var counter = 1;
     var newMatrix = [];
 
@@ -53,25 +52,27 @@ MatrixRotator.prototype.rotate = function(direction) {
       newMatrix.push(row);
     }
     SCHEMA = schema;
+    console.log('SCHEMA', SCHEMA);
     return newMatrix;
   }
 
 
-  function turnCW(matrix, layer) {
+  function turnCW(matrix, layer, dimensions) {
     layer = layer || 0;
+    var matrixDimensions = dimensions;
     var newMatrix = matrix;
-    var FIRST_INDEX = 0 + layer;
-    console.log('matrix', matrix);
-    console.log('SCHEMA', SCHEMA);
-    var LAST_INDEX = matrixDimensions - 1 - layer;
-    for (var i = 0 + layer; i < matrixDimensions - layer; i++) {
-      for (var j = 0 + layer; j < matrixDimensions - layer; j++) {
+    var FIRST_INDEX = 0;
+    var LAST_INDEX = matrixDimensions - 1;
+    for (var i = 0; i < matrixDimensions; i++) {
+      for (var j = 0; j < matrixDimensions; j++) {
         switch (true) {
           case i === FIRST_INDEX:
             if (j === FIRST_INDEX) {
               newMatrix[i][j] += matrixDimensions;
             } else {
               newMatrix[i][j] -= 1;
+
+
             }
             break;
 
@@ -79,7 +80,54 @@ MatrixRotator.prototype.rotate = function(direction) {
             if (j === FIRST_INDEX) {
               newMatrix[i][j] += matrixDimensions;
             }
-            if (j === matrixDimensions - 1 - layer) {
+            if (j === LAST_INDEX) {
+              newMatrix[i][j] -= matrixDimensions;
+            }
+            break;
+
+          case i === LAST_INDEX:
+            if (j >= FIRST_INDEX && j < LAST_INDEX) {
+              console.log('j', j);
+              newMatrix[i][j] += 1
+            }
+            if (j === LAST_INDEX) {
+              console.log('in');
+              newMatrix[i][j] -= matrixDimensions;
+            }
+        }
+        //end j fo
+      }
+      //end i for
+    }
+    return newMatrix;
+  }
+
+
+  /*  function turnCW(matrix, layer, dimensions) {
+    layer = layer || 0;
+    var matrixDimensions = dimensions;
+    var newMatrix = matrix;
+    var FIRST_INDEX = 0;
+    var LAST_INDEX = matrixDimensions-1;
+    for (var i = 0; i < matrixDimensions; i++) {
+      for (var j = 0; j < matrixDimensions; j++) {
+        switch (true) {
+          case i === FIRST_INDEX:
+            if (j === FIRST_INDEX) {
+              newMatrix[i][j] += matrixDimensions;
+
+            } else {
+              newMatrix[i][j] -= 1;
+
+
+            }
+            break;
+
+          case i > FIRST_INDEX && i < LAST_INDEX:
+            if (j === FIRST_INDEX) {
+              newMatrix[i][j] += matrixDimensions;
+            }
+            if (j === LAST_INDEX) {
               newMatrix[i][j] -= matrixDimensions;
             }
             break;
@@ -90,6 +138,7 @@ MatrixRotator.prototype.rotate = function(direction) {
               newMatrix[i][j] += 1
             }
             if (j === LAST_INDEX) {
+              console.log('in');
               newMatrix[i][j] -= matrixDimensions;
             }
         }
@@ -99,6 +148,7 @@ MatrixRotator.prototype.rotate = function(direction) {
     }
     return newMatrix;
   }
+*/
 
   function matchMatrixToSchema(matrix) {
     var newMatrix = matrix;
